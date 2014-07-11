b345f93b632bee326037e7503791c9f2c63b1798
Markus J Doetsch <git@the-enemy.org>
2014-07-11 19:28:42 +0200

    add example

---
+++ example.js
@@ -0,0 +1,17 @@
+/**
+ * example module
+ * @author Markus J Doetsch mdular.com
+ */
+/* global app:true */
+
+app.registerModule('example', function () {
+    "use strict";
+
+    var init = function () {
+        //console.log('example module init');
+    };
+
+    return {
+        init: init
+    };
+});
